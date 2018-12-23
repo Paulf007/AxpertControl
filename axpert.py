2a0795cdba2d7280145e2e4f3f09eba848265a74
Paulf007 <paul@paulf.co.za>
2018-12-23 08:52:46 +0200

    Update axpert.py

--- axpert.py
+++ axpert.py
@@ -9,7 +9,6 @@
 # real PL2303 = big trouble in my setup, cheap chinese converter some times disconnecting, workaround is at the end of serial_command(command) function
 # differenc between SBU(POP02) and Solar First (POP01): in state POP01 inverter works only if PV_voltage <> 0 !!! SBU mode works during night
 
-# Josef Krieglstein 20170215 last update
 
 import urllib2
 import serial, time, sys, string
@@ -22,21 +21,25 @@ import calendar
 import os
 import re
 import crcmod
+#import datetime as dt
 from binascii import unhexlify
+
+
 
 # Domain you want to post to: localhost would be an emoncms installation on your own laptop
 # this could be changed to emoncms.org to post to emoncms.org or your own server
-server = "emoncms.trenet.org"
+server = "localhost"
+
 
 # Location of emoncms in your server, the standard setup is to place it in a folder called emoncms
 # To post to emoncms.org change this to blank: ""
-emoncmspath = ""
+emoncmspath = "emoncms"
 
 # Write apikey of emoncms account
-apikey = "53863683c9745504ab727de6f736c94b"
+apikey = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 # Node id youd like the emontx to appear as
-nodeid = 13
+nodeid = 0
 
 #Axpert Commands and examples
 #QPI            # Device protocol ID inquiry
@@ -87,14 +90,16 @@ nodeid = 13
 #PPCP001        # Setting parallel device charger priority: SolarFirst - notworking
 #PPCP002        # Setting parallel device charger priority: OnlySolarCharging - notworking
 
+# most of the code is tripped down from the original and extra commands added to upload the maximum amount of data to emoncms.
+
 ser = serial.Serial()
-ser.port = "/dev/ttyUSB0"
+ser.port = "/dev/ttyUSB1"
 ser.baudrate = 2400
 ser.bytesize = serial.EIGHTBITS     #number of bits per bytes
 ser.parity = serial.PARITY_NONE     #set parity check: no parity
 ser.stopbits = serial.STOPBITS_ONE  #number of stop bits
-ser.timeout = 1                     #non-block read
 ser.xonxoff = False                 #disable software flow control
+ser.timeout = 1                     #non-block read
 ser.rtscts = False                  #disable hardware (RTS/CTS) flow control
 ser.dsrdtr = False                  #disable hardware (DSR/DTR) flow control
 ser.writeTimeout = 2                #timeout for write
@@ -105,6 +110,9 @@ try:
 except Exception, e:
     print "error open serial port: " + str(e)
     exit()
+
+
+
 
 def get_data():
     #collect data from axpert inverter
@@ -119,15 +127,30 @@ def get_data():
             else:
                 response.rstrip()
                 nums = response.split(' ', 99)
-                if nums[2] == "L":
-                    data += "Gridmode:1"
+## Get Inverter Watts from data
+		data += "Test:" + nums[15]
+		data += ",INVERTER_Total_VA:" + nums[16]
+		data += ",INVERTER_Total_W:" + nums[17]
+		data += ",INVERTER_Total_LOAD:" + nums[18]
+		data += ",INVERTER_MAX_CHARGE_RANGE:" + nums[23]
+		data += ",INVERTER_MAX_CHARGE:" + nums[22]
+		data += ",INVERTER_CHARGER_SOURCE:" + nums[21]
+## Log Grid data if inverter is switched to grid mode		
+		if nums[2] == "L":
+		    data += ",Gridmode:1"
+		    data += ",MAINS_WATT:" + nums[9]
                 else:
-                    data += "Gridmode:0"
-
-                if nums[2] == "B":
+                    data += ",Gridmode:0"
+		    data += ",MAINS_WATT:0"
+## Check if Panels is Active or not 
+		if nums[2] == "B":
                     data += ",Solarmode:1"
                 else:
                     data += ",Solarmode:0"
+
+
+
+## end of custom code _ Paul 
 
             response = serial_command("QPIGS")
             if "NAKss" in response:
@@ -136,23 +159,114 @@ def get_data():
                 return ""
             response.rstrip()
             nums = response.split(' ', 99)
-            data += ",Grid_voltage:" + nums[0]
-            data += ",Grid_frequency:" + nums[1]
-            data += ",AC_output_voltage:" + nums[2]
-            data += ",AC_output_frequency:" + nums[3]
-            data += ",AC_output_apparent_power:" + nums[4]
-            data += ",AC_output_active_power:" + nums[5]
-            data += ",Output_Load_Percent:" + nums[6]
-            data += ",Bus_voltage:" + nums[7]
-            data += ",Battery_voltage:" + nums[8]
-            data += ",Battery_charging_current:" + nums[9]
-            data += ",Battery_capacity:" + nums[10]
-            data += ",Inverter_heatsink_temperature:" + nums[11]
-            data += ",PV_input_current_for_battery:" + nums[12]
-            data += ",PV_input_voltage:" + nums[13]
-            data += ",Battery_voltage_from_SCC:" + nums[14]
-            data += ",Battery_discharge_current:" + nums[15]
-            data += ",Device_status:" + nums[16]
+	    #Calculations to be sent to Emon
+	    #set time
+            w = time.strftime("%H.%M")
+	    #Calculations
+	    powerf = float(nums[5])/float(nums[4])
+	    x = float(nums[8])*float(nums[12])
+	    y = float(nums[9])+float(nums[15])
+	    z = float(nums[5])/float(nums[2])
+ ## end of custom code _ Paul            
+            
+	    # Data that comes from the Inverter:		
+	    data += ",MAINS_V:" + nums[0]
+            data += ",MAINS_FREQ:" + nums[1]
+            data += ",INVERTER_VOLTS:" + nums[2]
+            data += ",INVERTER_FREQ:" + nums[3]
+            data += ",INVERTER_VA:" + nums[4]
+            data += ",INVERTER_WATTS:" + nums[5]
+            data += ",LOAD_PERCENTAGE:" + nums[6]
+            data += ",DC_BUS_VOLTS:" + nums[7]
+            data += ",BATTERY_VOLT:" + nums[8]
+            data += ",BATT_CHARGE_AMPS:" + nums[9]
+            data += ",BATT_SOC_STATE:" + nums[10]
+            data += ",HEATSYNC_TEMP:" + nums[11]
+            data += ",PANEL_AMPS:" + nums[12]
+            data += ",PANEL_VOLTAGE:" + nums[13]
+            data += ",SCC_DC_VOLTS:" + nums[14]
+            data += ",BATT_DISCHARGE_AMPS:" + nums[15]
+            #data += ",STATUS:" + nums[16]
+	    #Calculated values sent to emon
+			data += ",24_HOUR_TIME:" +  str(w)
+			data += ",PANEL_WATTS_OLD:" +  str(x)
+			data += ",BATTERY_CURRENT:" +  str(y)
+			data += ",INVERTER_CURRENT:" +  str(z)
+			data += ",INVERTER_POWER_FACTOR:" + str(powerf)
+		#Blank Values to Emon for custom inputs
+			data += ",USER_1:0"
+			data += ",USER_2:0"
+			data += ",USER_3:0"
+			data += ",USER_4:0"
+			data += ",USER_5:0" 
+## end of custom code _ Paul         
+		#Split Day and Night Use from inverter watts and solar watts
+	    if x > 0 :
+	    	data += ",INV_Night_Use:0"
+	    	data += ",INV_Day_Use:" + nums[5]
+            else:
+            	data += ",INV_Night_Use:" + nums[5]
+	   	data += ",INV_Day_Use:0"
+## end of custom code _ Paul 
+## New Command for custom Q1 Command _ Paul
+            response = serial_command("Q1")
+            if "NAKss" in response:
+                time.sleep(2)
+                return ""
+            else:
+                response.rstrip()
+                nums = response.split(' ', 99)
+## Get Data
+                data += ",V_Axpert_Firmware:" + nums[0]
+                data += ",V_BUS_Firmware:" + nums[1]
+                data += ",V_PWM_Temp:" + nums[5]
+                data += ",V_INVERTER_Temp:" + nums[6]
+                data += ",V_INVERTER_Batt_Temp:" + nums[7]
+                data += ",V_INVERTER_Transformer_Temp:" + nums[8]
+                data += ",PANEL_WATTS:" + nums[13]
+                data += ",V_INVERTER_Charge_Status:" + str(re.findall("\d+", nums[16]))
+
+## New Command for QPIRI Command _ Paul
+            response = serial_command("QPIRI")
+            if "NAKss" in response:
+                time.sleep(2)
+                return ""
+            else:
+                response.rstrip()
+                nums = response.split(' ', 99)
+## Get Data
+            data += ",xb_Grid_Rating_Volatge:" + nums[0]
+            data += ",xc_Grid_Rating_Current:" + nums[1]
+            data += ",xd_Inverter_Rating_Volatge:" + nums[2]
+            data += ",xe_Inverter_Rating_Freq:" + nums[3]
+            data += ",xf_Inverter_Rating_Current:" + nums[4]
+            data += ",xh_Inverter_Rating_VA:" + nums[5]
+            data += ",xi_Inverter_Rating_W:" + nums[6]
+			data += ",xj_Inverter_Batt_Rate_V:" + nums[7]
+			data += ",xk_Inverter_Batt_Recharge_V:" + nums[8]
+			data += ",xl_Inverter_Batt_Under_V:" + nums[9]
+			data += ",xm_Inverter_Batt_Bulk_V:" + nums[10]
+			data += ",xn_Inverter_Batt_Flout_V:" + nums[11]
+	        data += ",xo_Inverter_Batt_Type:" + nums[12]
+	        data += ",xp_Inverter_Grid_Charge:" + nums[13]
+	        data += ",xq_Inverter_Max_Charge:" + nums[14]
+	        data += ",xo_Inverter_Change_Mode:" + nums[15]
+	        data += ",xp_Inverter_Output_Source:" + nums[16]
+	        data += ",xs_Inverter_Charge_Source:" + nums[17]
+	        data += ",xt_Inverter_Type:" + nums[19]
+	        data += ",xv_Inverter_Battery_re_Discharge_v:" + nums[22]
+	        data += ",xx_Inverter_PV_Power_Balance:" + nums[23]
+# check for battery type Code did not work and still need to be fine tuned 
+		#if nums[13] == "AGM":
+		 #   data += ",x_Inverter_AGM:1"
+		  #  data += ",x_Inverter_Flooded:0"
+		# elif:
+               # if nums[13] == "Flooded":
+		#    data += ",x_Inverter_Flooded:1"
+	          #  data += ",x_Inverter_AGM:0"
+
+
+
 
         except Exception, e:
             print "error parsing inverter data...: " + str(e)
@@ -165,163 +279,7 @@ def get_data():
         return ""
     return data
 
-def set_charge_current():
-    # Automaticly adjust axpert inverter grid charging current
 
-    # 2A = 100W, 10A = 500W, 20A = 1000W, 30 = 1500W
-    # load >3000W -> 02A
-    # load <3000W -> 10A
-    # load <2000W -> 20A
-    # load <1000W -> 30A
-
-    if ser.isOpen():
-        try:
-            current = 0
-            load_power = 0
-            response = serial_command("QPGS0")
-            if "NAKss" in response:
-                time.sleep(0.5)
-                return [0]
-            response.rstrip()
-            nums = response.split(' ', 99)
-            current = int ( nums[24] )
-            response = serial_command("QPIGS")
-            if "NAKss" in response:
-                time.sleep(0.5)
-                return [0]
-            response.rstrip()
-            nums = response.split(' ', 99)
-            load_power = int ( nums[5] )
-            print load_power
-            if load_power > 3000:
-                if not current == 2:
-                    current = 2
-                    response = serial_command("MUCHGC002")
-            elif load_power > 2000:
-                if not current == 10:
-                    current = 10
-                    response = serial_command("MUCHGC010")
-            elif load_power > 1000:
-                if not current == 20:
-                    current = 20
-                    response = serial_command("MUCHGC020")
-            else:
-                if not current == 30:
-                    current = 30
-                    response = serial_command("MUCHGC030")
-            print current
-            if "NAKss" in response:
-                time.sleep(0.5)
-                return [0]
-
-        except Exception, e:
-            print "error parsing inverter data...: " + str(e)
-            return [0]
-
-    else:
-        ser.close()
-        print "cannot use serial port ..."
-        return [0]
-    return current
-
-def get_output_source_priority():
-    #get inverter output mode priority
-    output_source_priority = "8"
-    if ser.isOpen():
-        try:
-            response = serial_command("QPIRI")
-            if "NAKss" in response:
-                time.sleep(0.5)
-                return ""
-            response.rstrip()
-            nums = response.split(' ', 99)
-            output_source_priority = nums[16]
-
-        except Exception, e:
-            print "error parsing inverter data...: " + str(e)
-            return ""
-
-    else:
-        ser.close()
-        print "cannot use serial port ..."
-        return ""
-    return output_source_priority
-
-def get_charger_source_priority():
-    #get inverter charger mode priority
-    charger_source_priority = "8"
-    if ser.isOpen():
-        try:
-            response = serial_command("QPIRI")
-            if "NAKss" in response:
-                time.sleep(0.5)
-                return ""
-            response.rstrip()
-            nums = response.split(' ', 99)
-            charger_source_priority = nums[17]
-
-        except Exception, e:
-            print "error parsing inverter data...: " + str(e)
-            return ""
-
-    else:
-        ser.close()
-        print "cannot use serial port ..."
-        return ""
-    return charger_source_priority
-
-def set_output_source_priority(output_source_priority):
-    #set inverter output mode priority
-        if not output_source_priority == "":
-            if ser.isOpen():
-                try:
-                    if output_source_priority == 0:
-                        response = serial_command("POP00")
-                        print response
-                    elif output_source_priority == 1:
-                        response = serial_command("POP01")
-                        print response
-                    elif output_source_priority == 2:
-                        response = serial_command("POP02")
-                        print response
-
-                except Exception, e:
-                    print "error parsing inverter data...: " + str(e)
-                    return [0]
-
-        else:
-            ser.close()
-            print "cannot use serial port ..."
-            return [0]
-        return [1]
-
-def set_charger_source_priority(charger_source_priority):
-    #set inverter charge mode priority
-        if not charger_source_priority == "":
-            if ser.isOpen():
-                try:
-                    if charger_source_priority == 0:
-                        response = serial_command("PCP00")
-                        print response
-                    elif charger_source_priority == 1:
-                        response = serial_command("PCP01")
-                        print response
-                    elif charger_source_priority == 2:
-                        response = serial_command("PCP02")
-                        print response
-                    elif charger_source_priority == 3:
-                        response = serial_command("PCP03")
-                        print response
-
-                except Exception, e:
-                    print "error parsing inverter data...: " + str(e)
-                    return [0]
-
-        else:
-            ser.close()
-            print "cannot use serial port ..."
-            return [0]
-        return [1]
 
 def send_data(data):
     # Send data to emoncms server
@@ -336,23 +294,7 @@ def send_data(data):
         return [0]
     return [1]
 
-def read_hdo(id):
-    # Read high/low tarif from emoncms (the acctual tarif information can be created by cron script, or from emonTX input)
-    # in Czech Republic, West Bohemia it is perriodicaly, tarif name is for example: A1B8DP5 => (8 hours of low and 16 hours of high) / each day
-    try:
-        conn = httplib.HTTPConnection(server)
-        conn.request("GET", "/"+emoncmspath+"/feed/value.json?id="+str(id)+"&apikey="+apikey)
-        response = conn.getresponse()
-        response_tmp = response.read()
-        conn.close()
-        return response_tmp
-
-    except Exception as e:
-        print "error reading from emoncms...: " + str(e)
-        return [0]
-    return [1]
-
-
+# Run Serial Command
 def serial_command(command):
     try:
         ser.flushInput()            #flush input buffer, discarding all its contents
@@ -383,27 +325,10 @@ def main():
     while True:
         time.sleep(0.1)
         data = get_data()
-        charge_current = set_charge_current ()
+##        charge_current = set_charge_current ()
         if not data == "":
             send = send_data(data)
-        hdo_tmp_LT = read_hdo(68)       #Read emoncms feed id=68 = LowTarif
-        hdo_tmp_HT = read_hdo(69)       #Read emoncms feed id=69 = HighTarif
-        output_source_priority = get_output_source_priority()
-        charger_source_priority = get_charger_source_priority()
-        if not output_source_priority == "8":
-            if not charger_source_priority == "8":
-                if hdo_tmp_LT == "1":
-                    print "LT"  # electricity is cheap, so charge batteries from grid and hold them fully charged! important for Lead Acid Batteries Only!
-                    if not output_source_priority == "0":       # Utility First (0: Utility first, 1: Solar First, 2: SBU)
-                        set_output_source_priority(0)
-                    if not charger_source_priority == "2":      # Utility First (0: Utility first, 1: Solar First, 2: Solar+Utility, 3: Solar Only)
-                        set_charger_source_priority(2)
-                if hdo_tmp_HT == "1":
-                    print "HT"  # electricity is expensive, so supply everything from batteries not from grid
-                    if not output_source_priority == "2":       # Utility First (0: Utility first, 1: Solar First, 2: SBU)
-                        set_output_source_priority(2)
-                    if not charger_source_priority == "3":      # Utility First (0: Utility first, 1: Solar First, 2: Solar+Utility, 3: Solar Only)
-                        set_charger_source_priority(3)
 
 if __name__ == '__main__':
     main()
+
